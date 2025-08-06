9c830a3b470243c3fce5ae099a4c73dd0a63b577
Ankittt81 <121846044+Ankittt81@users.noreply.github.com>
2025-08-06 13:35:04 +0530

    push

--- Client/Movie Book/src/components/ProtectedRoute.jsx
+++ Client/Movie Book/src/components/ProtectedRoute.jsx
@@ -1,13 +1,68 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { GetCurrentUser } from "../apiCalls/user";
 import { setUser } from "../redux/userSlice";
+import { Layout, message } from "antd";
+
+import { Header } from "antd/es/layout/layout";
+import {
+  HomeOutlined,
+  LogoutOutlined,
+  ProfileOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+
+import { Link } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
   const { user } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const navItems = [
+    {
+      label: "Home",
+      icon: <HomeOutlined />,
+    },
+    {
+      label: `${user ? user.name : null}`,
+      icon: <UserOutlined />,
+      children: [
+        {
+          label: (
+            <span
+              onClick={() => {
+                if (user.role === "admin") {
+                  navigate("/admin");
+                } else if (user.role === "partner") {
+                  navigate("/partner");
+                } else {
+                  navigate("/profile");
+                }
+              }}
+            >
+              My Profile
+            </span>
+          ),
+          icon: <ProfileOutlined />,
+        },
+        {
+          label: (
+            <Link
+              to="/login"
+              onClick={() => {
+                localStorage.removeItem("token");
+              }}
+            >
+              Log Out
+            </Link>
+          ),
+          icon: <LogoutOutlined />,
+        },
+      ],
+    },
+  ];
   const getValidUser = async () => {
     try {
       const response = await GetCurrentUser();
@@ -22,12 +77,38 @@ function ProtectedRoute({ children }) {
   };
   useEffect(() => {
     if (localStorage.getItem("token")) {
-      getValidUser()
+      getValidUser();
     } else {
       navigate("/login");
     }
   }, []);
-  return <div>{children}</div>;
+  return (
+    user && (
+      <>
+        <Layout>
+          <Header
+            className="d-flex justify-content-between"
+            style={{
+              position: "sticky",
+              top: 0,
+              zindex: 1,
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <h3 className="demo-logo text-white m-0 style={{color:'white'}}">
+              Book My Show
+            </h3>
+            <Menu theme="dark" mode="horizontal" items={navItems} />
+          </Header>
+          <div style={{ padding: 24, minHeight: 380, background: "#fff" }}>
+            {children}
+          </div>
+        </Layout>
+      </>
+    )
+  );
 }
 
 export default ProtectedRoute;
